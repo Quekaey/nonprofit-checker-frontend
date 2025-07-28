e7a881fcf843c6b5f8f2cd28159781ff728ec42a
Felix <feudeine@sevnbytes.com>
2025-07-28 02:23:19 +0100

    fixed frontend bugs

--- src/main.server.ts
+++
@@ -1,6 +0,0 @@
-import 'zone.js/node';
-import { renderModule } from '@angular/platform-server';
-import { AppServerModule } from './app/app.server.module';
-
-// re-export only what actually exists
-export { AppServerModule, renderModule };
